ca823bca48ecec66edae36a6a65c05a91acfa959
Seth Nelson <seth.nelson@outlook.com>
2020-07-14 16:49:00 -0700

    finished file.io excercise

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open('foo.txt') as f: # ('foo.txt', 'r') <--- will be 'r' as default for Read file
+    print(f.read())
+
+# Check that file is closed
+# f.closed <--- only needed if not using 'with'
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as f: # 'W' is for writing a file (creating new one)
+    f.write('Shoo foo\n')
+    f.write('Shoo who?\n')
+    f.write('You foo!\n')
